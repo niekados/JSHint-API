ea87ece4ed8a04ed14ad0327695d33028c3940db
niekados <tiny.post@yahoo.com>
2024-05-03 17:05:28 +0000

    Add displayExceptions function to script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -47,6 +47,7 @@ async function postForm(e) {
     if (response.ok) {
         displayErrors(data);
     } else {
+        displayException(data);
         throw new Error(data.error);
     }
 }
@@ -83,6 +84,7 @@ async function getStatus(e) {
     if (response.ok) {
         displayStatus(data);
     } else {
+        displayException(data);
         throw new Error(data.error);
     }
 }
@@ -96,5 +98,16 @@ function displayStatus(data) {
     document.getElementById("resultsModalTitle").innerText = heading;
     document.getElementById("results-content").innerHTML = results;
 
+    resultsModal.show();
+}
+
+function displayException(data) {
+    let heading = `An Exception Occurred`
+    results = `<div>The API returned status code ${data.status_code}</div>`
+    results += `<div>Error number: <strong>${data.error_no}</strong></div>`
+    results += `<div>Error text: <strong>${data.error}</strong></div>`
+
+    document.getElementById("resultsModalTitle").innerText = heading;
+    document.getElementById("results-content").innerHTML = results;
     resultsModal.show();
 }
